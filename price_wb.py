b318e0eac596c5c75691c38363c6d0f69345047d
usiqwerty <benlogin1@protonmail.com>
2023-11-03 19:55:39 +0500

    Save prices with timestamp

--- price_wb.py
+++ price_wb.py
@@ -1,9 +1,10 @@
 #import telebot
 import json
-import time
+import time, datetime
 import requests
+from collections import namedtuple
 
-#необходимо указать токен своего бота, созданного через Botfather
+Product = namedtuple("Product", ["name", "price", "brand"])
 #bot = telebot.TeleBot('TOKEN')
 
 filename_articles = "articles.json"
@@ -16,39 +17,46 @@ def load_from_file(filename):
         data = None
     return data
 
-def mainloop():
-    if not articles:
-        print(f"Empty articles list. Fill {filename_articles}")
-    while articles:
-        for article in articles:
-            url = f'https://card.wb.ru/cards/detail?spp=27&nm={article}'
+def fetch_info_by_article(article):
+    url = f'https://card.wb.ru/cards/detail?spp=27&nm={article}'
 
-            response = requests.get(url)
-            if response.status_code != 200:
-                print(response)
-                break
-            response_json = response.json()
-            products = response_json['data']['products']
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(response)
+        return None
+    response_json = response.json()
+    products = response_json['data']['products']
 
-            product = products[0]
-            title = product['name']
-            brand = product['brand']
-            price = product['salePriceU'] / 100
+    product = products[0]
+    title = product['name']
+    brand = product['brand']
+    price = product['salePriceU'] / 100
 
-            if title in prices:
-                    print(f'Товар "{title}" (от {brand}) подешевел на {prices[title] - price} рублей.\n'
-                          f'Ссылка на товар: https://www.wildberries.ru/catalog/{article}/detail.aspx')
-            prices[title] = price
-
-            print("Gone sleep")
-            time.sleep(420)
+    print(f'Ссылка на товар: https://www.wildberries.ru/catalog/{article}/detail.aspx')
+    return Product(title, price, brand)
 
 
 articles = load_from_file(filename_articles) or []
 prices = load_from_file(filename_prices) or {}
+
 try:
-    mainloop()
+    today = datetime.datetime.today().strftime("%d.%m.%Y")
+    if not articles:
+        print(f"Empty articles list. Fill {filename_articles}")
+    while articles:
+        for article in articles:
+            article = str(article)
+            product = fetch_info_by_article(article)
+
+            if article not in prices:
+                prices[article]={}
+            prices[article][today] = product.price
+
+            print("Gone sleep")
+            time.sleep(420)
 except KeyboardInterrupt:
     print("Stopped by keyboard")
-with open(filename_prices, 'w', encoding='utf-8') as write_test:
-    json.dump(prices, write_test, ensure_ascii=False)
+finally:
+    print(prices)
+    with open(filename_prices, 'w', encoding='utf-8') as write_test:
+        json.dump(prices, write_test, ensure_ascii=False)
