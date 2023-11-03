e00cfde33d86e088e738d33ffd30b5d83d4e564d
usiqwerty <benlogin1@protonmail.com>
2023-11-03 18:57:30 +0500

    Refactored file operations. Naming refactoring

--- price_wb.py
+++ price_wb.py
@@ -6,42 +6,50 @@ import requests
 #необходимо указать токен своего бота, созданного через Botfather
 #bot = telebot.TeleBot('TOKEN')
 
-filename = "articles.json"
+filename_articles = "articles.json"
+filename_prices = "prices.json"
 try:
-    with open(filename, encoding='utf-8') as read_art:
+    with open(filename_articles, encoding='utf-8') as read_art:
         articles = json.load(read_art)
 except:
     articles = []
-while True:
-    
 
+try:
+    with open(filename_prices, 'r', encoding='utf-8') as read_test:
+        prices = json.load(read_test)
+except:
+    prices = {}
 
-    for elem in articles:
-        url = 'https://card.wb.ru/cards/detail?spp=27&nm='
-        r = requests.get(f"{url}{elem}").content
-        
-        text = json.loads(r)
-        
-        title = text.get('data').get('products')[0].get('name')
-        price = text.get('data').get('products')[0].get('salePriceU')//100
-        average_price = text.get('data').get('products')[0].get('averagePrice', 0)//100
-        benefit = text.get('data').get('products')[0].get('benefit')
-        
-        with open ('prices.json', 'r', encoding='utf-8') as read_test:
-            dict = json.load(read_test)
-            if dict.get(title):
-                if dict[title] > price:
-                    print(f'Товар {title} подешевел на {dict[title] - price} рублей. \n'
+def mainloop():
+    while True:
+        for elem in articles:
+            url = 'https://card.wb.ru/cards/detail?spp=27&nm='
+            r = requests.get(f"{url}{elem}").content
+
+            text = json.loads(r)
+
+            title = text.get('data').get('products')[0].get('name')
+            price = text.get('data').get('products')[0].get('salePriceU')//100
+            average_price = text.get('data').get('products')[0].get('averagePrice', 0)//100
+            benefit = text.get('data').get('products')[0].get('benefit')
+
+            if prices.get(title):
+                if prices[title] > price:
+                    print(f'Товар {title} подешевел на {prices[title] - price} рублей. \n'
                                                        f'Актуальная цена: {price}.\n'
                                                        f'Средняя цена на данный товар на ВБ: {average_price}\n'
                                                        f'Выгода: {benefit}%\n'
                                                        f'Ссылка на товар: https://www.wildberries.ru/catalog/{elem}/detail.aspx')
-                    dict[title] = price
-                    
+                    prices[title] = price
             else:
-                dict[title] = price
-                
-        with open('prices.json', 'w', encoding='utf-8') as write_test:
-            json.dump(dict, write_test, ensure_ascii=False)
-        print("Gone sleep")
-        time.sleep(420)
+                prices[title] = price
+
+            print("Gone sleep")
+            time.sleep(420)
+
+try:
+    mainloop()
+except KeyboardInterrupt:
+    pass
+with open(filename_prices, 'w', encoding='utf-8') as write_test:
+    json.dump(prices, write_test, ensure_ascii=False)
